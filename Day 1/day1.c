5d384e1bca9cd03ca85504f4190610d4f06bac79
gkwityn <gkwityn@gmail.com>
2021-12-14 17:20:04 -0500

    day1 init

---
+++ Day 1/day1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int const depth_readings[7];
+    int data;
+
+    FILE *fp = ("sonar_sweep", "r");
+    fopen(fp);
+
+    while(fp != NULL){
+        
+    }
+    fscanf("%d", depth_readings[]);
+
+
+
+
+    return 0;
+}
+
