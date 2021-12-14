11b5b32542de23e5e29412c82b7f014fcd1c62c9
gkwityn <gkwityn@gmail.com>
2021-12-14 18:39:13 -0500

    day1 complete

--- Day 1/day1.c
+++ Day 1/day1.c
@@ -1,21 +1,41 @@
+#include <stdlib.h>
 #include <stdio.h>
+
+#define N_INPUT 2000
 
 int main(int argc, char const *argv[])
 {
-    int const depth_readings[7];
+    int depth_readings[N_INPUT];
     int data;
+    int count = 0;
 
-    FILE *fp = ("sonar_sweep", "r");
-    fopen(fp);
 
-    while(fp != NULL){
-        
+
+    FILE *fp;
+    fp = fopen("sonar_sweep.txt", "r");
+    if(fp == NULL){
+        return EXIT_FAILURE;
+    } 
+
+
+    int i = 0;
+    while( fscanf(fp, "%d", &data) != EOF ){
+        depth_readings[i++] = data;  
     }
-    fscanf("%d", depth_readings[]);
 
 
+    
+    int prev = depth_readings[0];
 
+    for(int i = 1; i < N_INPUT; i++){
+        if(depth_readings[i] > prev){
+            count++;
+        }
+        prev = depth_readings[i];
+    }
 
-    return 0;
+    printf("Number of depth increases: %d\n", count);
+
+    return EXIT_SUCCESS;
 }
 
