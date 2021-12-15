dffc1fbb287de164d3a95f5969a6a9c9df8ff197
gkwityn <gkwityn@gmail.com>
2021-12-15 12:23:59 -0500

    FIXME wrong output

--- day_2/part2/day2_pt2.c
+++ day_2/part2/day2_pt2.c
@@ -1,58 +1,78 @@
+
+/*
+forward X increases the horizontal position by X units.
+down X increases the depth by X units.
+up X decreases the depth by X units.
+    
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+*/
+
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct sub{
+
+
+struct submarine{
     int horizontal;
     int depth;
     int aim;
 };
 
 
-int main(int argc, char const *argv[])
-{
-    int horizontal = 0;
-    int depth = 0;
+
+int main(int argc, char const *argv[]){
+    
+    struct submarine my_sub;
+
+    my_sub.horizontal = 0;
+    my_sub.depth = 0;
+    my_sub.aim = 0;    
+    
 
     char buff[8];
     int x_unites;
-    
-    /*
-    forward X increases the horizontal position by X units.
-    down X increases the depth by X units.
-    up X decreases the depth by X units.
-        
-    down X increases your aim by X units.
-    up X decreases your aim by X units.
-    forward X does two things:
-    It increases your horizontal position by X units.
-    It increases your depth by your aim multiplied by X.
-    */
 
     FILE *fp;
     fp = fopen("input.txt", "r");
+    if(fp == NULL){
+        printf("Error: unable to open file.\n");
+        return EXIT_FAILURE;
+    }
+    
+    //int lines = 0;
 
     while(fscanf(fp, "%s %d", buff, &x_unites) != EOF){
         //printf("%s  %d\n", buff, x_unites);
         //printf("Hoz: %d, Depth: %d\n", horizontal, depth);
+        //lines++;
+        
 
         if(strcmp(buff, "forward") == 0){
-            horizontal += x_unites;
-            //printf("horiz: %d\n", horizontal);
+            printf("Hoz: %d\n", my_sub.horizontal);
+            my_sub.depth += (my_sub.aim * x_unites);
+            my_sub.horizontal += x_unites;
+            
         }
         else if(strcmp(buff, "up") == 0){
-            depth -= x_unites;
+            my_sub.aim -= x_unites;
+            my_sub.depth-= x_unites;
             //printf("Up: %d\n", depth);
         }
         else if(strcmp(buff, "down") == 0){
-            depth += x_unites;
+            my_sub.aim += x_unites;
+            my_sub.depth += x_unites;
             //printf("Down: %d\n", depth);
         }
     }
 
-
-    printf("Horiz * Depth = %d\n", horizontal*depth);
-
-
+    //printf("Lines Read: %d\n", lines);
+    printf("Horiz * Depth = %d\n", my_sub.horizontal * my_sub.depth);
     
     fclose(fp);
     return 0;
