7a7846d75c203cdf1dff91d88eaec4cafd81beec
gkwityn <gkwityn@gmail.com>
2021-12-17 11:08:29 -0500

    fixing array bound bug

--- day_3/part1/day3_pt1.c
+++ day_3/part1/day3_pt1.c
@@ -1,19 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define BIT_LENGTH 13
 
 void build_gamma_epsilon(char *gamma, char *epsilon, int *ones, int *zeros){
 
-    for(int i = 0; i < BIT_LENGTH-1; i++){
-        if (ones[i] > zeros[i]){
+   
+
+    for(int i = 0; i <= BIT_LENGTH; i++){
+        
+
+
+        if(i == BIT_LENGTH){
+            gamma[i] = '\0';
+            epsilon[i] = '\0';
+        }
+        else if (ones[i] > zeros[i]){
             gamma[i] = '1';
             epsilon[i] = '0';
         }
-        else{
+        else if (zeros[i] > ones[i]){
             gamma[i] = '0';
             epsilon[i] = '1';
         }
+        
     }
 
     return;
@@ -24,12 +35,15 @@ void build_gamma_epsilon(char *gamma, char *epsilon, int *ones, int *zeros){
 int base10_to_binary(char *array){
 
     int n = 0;
-
     for(int i = BIT_LENGTH-2; i >= 0; i--){
         if(array[i] == '1'){
+            printf("sum: %d ", n);
             n += (1 << i);
+            
         }
     }
+    printf("sum: %d\n\n", n);
+
 
     return n;
 }
@@ -70,31 +84,37 @@ int main(int argc, char const *argv[])
     char gamma[BIT_LENGTH];
     char epsilon[BIT_LENGTH];
 
-    int ones[BIT_LENGTH];
-    int zeros[BIT_LENGTH];
+    int ones[BIT_LENGTH-1];
+    int zeros[BIT_LENGTH-1];
 
     
     for(int i = 0; i < BIT_LENGTH-1; i++){
         ones[i] = 0;
         zeros[i] = 0;
+        gamma[i] = '0';
+        epsilon[i] = '0';
     }
+        gamma[BIT_LENGTH] = '\0';
+        epsilon[BIT_LENGTH] = '\0';
     
     FILE *fp;
     fp = fopen("input.txt", "r");
 
     while(fscanf(fp, "%s", buff) != EOF){
         
-        for(int i = 0; i < BIT_LENGTH-1; i++){
+        for(int i = 0; i < BIT_LENGTH; i++){
+        
             if(buff[i] == '1'){
                 ones[i] = ones[i]+1;
             }else if(buff[i] == '0'){
                 zeros[i] = zeros[i]+1;
             }
+            
         }
     }
 
     
-    /*    
+    
     //print sum of occurances of 1 or 0 in
     //each index of corosponding arrays.
 
@@ -102,7 +122,7 @@ int main(int argc, char const *argv[])
     print_binary_int_array(ones);
     printf("zeros: ");
     print_binary_int_array(zeros);
-    */
+    
 
     build_gamma_epsilon(gamma, epsilon, ones, zeros);
     printf("gamma base2: ");
