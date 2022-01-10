ae4cea148a840afb42596344f19abd1a98fb65bb
gkwityn <gkwityn@gmail.com>
2022-01-10 01:52:52 -0500

    starting day3 pt1

---
+++ day_4/part1/day4_pt1.java
@@ -0,0 +1,8 @@
+public class day4_pt1 {
+    
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ day_4/part1/value.java
@@ -0,0 +1,28 @@
+public class value {
+
+    private int number;
+    private boolean status;
+
+
+    public value(int number, boolean status){
+        number = number;
+        status = status;
+    }
+
+    public void setNumber(int number){
+        number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setStatus(boolean bool){
+        status = bool;
+    }
+
+    public boolean getStatus(){
+        return status;
+    }
+
+}
