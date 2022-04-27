4f7ed38efa4fe8c53ee0e7dc1c9c225e20c8f7e9
gkwityn <gkwityn@gmail.com>
2022-04-27 16:45:17 -0400

    Restarting

--- day_5/part1/day5_pt1.java
+++ day_5/part1/day5_pt1.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class day5_pt1 {
 
-    public static void fileIO(ArrayList lineList, int x_max, int y_max) {
+    public static void fileIO() {
         
         try{
             File file = new File("sample_input.txt");
@@ -28,30 +28,41 @@ public class day5_pt1 {
                 x1 = Integer.parseInt(token.nextToken());
                 y1 = Integer.parseInt(token.nextToken());
                 x2 = Integer.parseInt(token.nextToken());                
-                y2 = Integer.parseInt(token.nextToken());                  
+                y2 = Integer.parseInt(token.nextToken());
                 
+                //Rearange line ends into increasing order.
+                if(x2 < x1){
+                    int tempX = x2;
+                    x2 = x1;
+                    x1 = tempX;
+                }
+                
+                if(y2 < y1){
+                    int tempY = y2;
+                    y2 = x1;
+                    y1 = tempY;
+                }
+                
+                //Build Start and End point Array:  ar[x,y]
+                int[]p_Start = new int[2];
+                p_Start[0]=x1;
+                p_Start[1]=y1;
+
+                int[]p_End = new int[2];
+                p_End[0] = x2;
+                p_End[1] = y2;
+
                 //Check for straight or vertial line
                 if(x1 == x2 || y1 == y2){
-                    LineObj myLine = new LineObj(x1,x2,y1,y2);                
-                    lineList.add(myLine);  
+                    //Build line in x direction
+                    if(x1 != x2){
+                        
+                    }
+                    
                 }
 
 
-                if(x1 >= x_max){
-                    x_max = x1;
-                }
-
-                if(x2 >= x_max){
-                    x_max = x2;
-                }
-
-                if(y1 >= y_max){                    
-                    y_max = y1;
-                }
-
-                if(y2 >= y_max){
-                    y_max = y2;
-                }
+                
             }
 
             scan.close();           
@@ -63,37 +74,13 @@ public class day5_pt1 {
     }
 
 
-    public static int findOverlap(int overlapSum, ArrayList<LineObj>lineList){
-        int inputSize = lineList.size();
-        
-        for(int i=0; i < inputSize; i++){
-            for(int j =0; j <inputSize; j++){
-
-                
-
-            }
-        }
-        return inputSize;
-    }
+   
 
 
     public static void main(String[] args) {
 
-        ArrayList<LineObj> lineList = new ArrayList<>();
-        
-        int x_max, y_max;
-        x_max = y_max = 0;
-        
-        
-        fileIO(lineList, x_max, y_max);
-
-        System.out.println(x_max + ", " + y_max);
-
-
-        
-
-        int overlapSum = 0;
-        findOverlap(overlapSum, lineList);      
+        fileIO();
+   
 
 
 
