fac73d3a223a74a98146da2fe435bede5203ec02
Greg Kwityn <gkwityn@gmail.com>
2022-04-28 00:00:30 -0400

    building grid

--- day_5/part1/day5_pt1.java
+++ day_5/part1/day5_pt1.java
@@ -11,12 +11,14 @@ import java.util.Scanner;
 
 public class day5_pt1 {
 
+    public static int xMax = 0;
+    public static int yMax = 0;
+
     public static void fileIO() {
         
         try{
             File file = new File("sample_input.txt");
             Scanner scan = new Scanner(file);            
-
             
 
             while(scan.hasNextLine()){
@@ -27,41 +29,43 @@ public class day5_pt1 {
                 
                 x1 = Integer.parseInt(token.nextToken());
                 y1 = Integer.parseInt(token.nextToken());
-                x2 = Integer.parseInt(token.nextToken());                
+                x2 = Integer.parseInt(token.nextToken());             
                 y2 = Integer.parseInt(token.nextToken());
-                
-                //Rearange line ends into increasing order.
-                if(x2 < x1){
-                    int tempX = x2;
-                    x2 = x1;
-                    x1 = tempX;
+
+               //System.out.println("Original");
+               //System.out.println(x1 + ", " + y1 + ", " + x2 + ", " + y2);
+
+                //Find the max for determining grid size.
+                xMax = findxMax(xMax, x1, x2);                
+                yMax = findxMax(yMax, y1, y2);
+
+                //Skip non Horizontal or vertical line.
+                if(x1 != x2 && y1 != y2){
+                    //System.out.printf("Skipping line\n\n");
+                    continue;
                 }
                 
-                if(y2 < y1){
-                    int tempY = y2;
-                    y2 = x1;
-                    y1 = tempY;
+                //Format coordinates to move in positive direction
+                if(x1 == x2){
+                    if(y1 > y2){
+                        int temp = y1;
+                        y1 = y2;
+                        y2 = temp;
+                    }              
                 }
-                
-                //Build Start and End point Array:  ar[x,y]
-                int[]p_Start = new int[2];
-                p_Start[0]=x1;
-                p_Start[1]=y1;
-
-                int[]p_End = new int[2];
-                p_End[0] = x2;
-                p_End[1] = y2;
-
-                //Check for straight or vertial line
-                if(x1 == x2 || y1 == y2){
-                    //Build line in x direction
-                    if(x1 != x2){
-                        
+                else if(y1 == y2){
+                    if(x1 > x2){
+                        int temp = x1;
+                        x1 = x2;
+                        x2 = temp;
                     }
-                    
                 }
 
-
+                //System.out.println("Updated");
+                //System.out.println(x1 + ", " + y1 + ", " + x2 + ", " + y2 +"\n");   
+                
+                int[][]point = new int[1][1];
+                ArrayList<int[][]> line = buildLine(line, x1, y1, x2, y2);
                 
             }
 
@@ -73,16 +77,57 @@ public class day5_pt1 {
         }
     }
 
+    //TODO
+    //Build the line arraylist
+    public static ArrayList buildLine(ArrayList line, int x1, int y1, int x2, inty2)){
 
+        return line;
+    }
+
+    public static int findxMax(int xMax, int x1, int x2){
+        
+        if( (x1 > xMax) | (x2 > xMax) ){
+            if(x1 > x2){
+                return x1;
+            }
+            else{
+                return x2;
+            }          
+        }
+        return xMax;
+    }
+
+    public static int findyMax(int yMax, int y1, int y2){
+        
+        if((y1 > yMax) | ( y2 > yMax)){
+            if(y1>y2){
+                return y1;
+            }else{
+                return y2;
+            }         
+        }
+        return yMax;
+    }
+
+    public static void print_grid(){
+        for(int x = 0; x < xMax; x++){
+            for(int y = 0; y < yMax; y++){
+                System.out.printf(".");
+            }
+            System.out.printf("\n");
+        }
+    }
    
 
 
     public static void main(String[] args) {
-
+        
         fileIO();
    
+        //System.out.println("Final xMAx: " + xMax);
+        //System.out.println("Final yMAx: " + yMax);
 
-
+        print_grid();
 
     }
         
